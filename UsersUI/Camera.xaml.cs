cffea936861ff5a4b8f53b47011eeeeebb4f29ef
https://github.com/BugaenkoVladislav <bugaenkovlad201@gmail.com>
2023-10-05 01:44:36 +0300

    added coments

--- UsersUI/Camera.xaml.cs
+++ UsersUI/Camera.xaml.cs
@@ -44,14 +44,14 @@ namespace UsersUI
                     //разделяем значения и добавляем в список students
                     foreach (JObject e in arr.Cast<JObject>())
                     {
-                        users.Add(new User(Convert.ToInt32(e["idUser"]), Convert.ToInt32(e["idStatus"]),30));//добавляем нового пользователя    
+                        users.Add(new User(Convert.ToInt32(e["idUser"]), Convert.ToInt32(e["idStatus"]),30));//добавляем нового пользователя    ["idUser"] - берется из поля Json в Swagger'e
                     }
                 }
                 
             }
             DrawUsers();
         }
-        private void DrawUsers()
+        private void DrawUsers()//отрисовывает пользователей
         {
             for(int i =0;i<users.Count;i++)
             {
